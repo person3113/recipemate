6475f6a6da76ecf24fd7f53f23c1d675d6d41a2c
Seongoh Jeong <person3113@gmail.com>
2025-10-26 05:40:36 +0900

    first commit

---
+++ src/test/java/com/recipemate/recipemate_api/RecipeMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipemate.recipemate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
